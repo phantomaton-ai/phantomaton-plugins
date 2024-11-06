cb999e3ed1ecf6ef4bc4a102b72be454e6128f5e
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-06 15:52:29 -0800

    reexpose

--- create.js
+++ create.js
@@ -4,7 +4,10 @@ const create = (components, instantiate, install) => {
   const extensions = expose(components);
   return configuration => {
     const instance = instantiate(configuration);
-    return { install: install({ configuration, extensions, instance }) };
+    return {
+      install: install({ configuration, extensions, instance }),
+      instance
+    };
   };
 };
 
