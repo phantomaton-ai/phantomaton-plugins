c5443bf25f5e52ee7d4947ccd4ba9f4381621bfd
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-06 15:42:09 -0800

    Updated by Phantomaton

--- create.js
+++ create.js
@@ -1,9 +1,11 @@
 import expose from './expose.js';
 
 const create = (components, instantiate, install) => {
-  const extensions = expose(a);
+  const extensions = expose(components);
   return configuration => {
     const instance = instantiate(configuration);
     return { install: install({ configuration, extensions, instance }) };
   };
 };
+
+export default create;
