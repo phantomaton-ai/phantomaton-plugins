90a219fb42e88d45f3869e73898d0058b63ac04d
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-06 14:17:51 -0800

    create

--- create.js
+++ create.js
@@ -1,3 +1,11 @@
 const create = (a, b, c) => {
-  
+  const extensions = expose(a);
+  const instantiate = configuration => b(configuration);
+  const install = plugin => c(plugin);
+  const plugin = { extensions: expose(a) };
+  return configuration => {
+    plugin.configuration = configuration;
+    plugin.instance = instantiate(configuration);
+    return { install: install(plugin) };
+  };
 };
