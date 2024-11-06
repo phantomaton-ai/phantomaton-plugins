9d79b2985b4c1c913fbd910f2816bf35beca6c46
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-06 15:50:01 -0800

    Updated by Phantomaton

---
+++ index.js
@@ -0,0 +1,12 @@
+import create from './create.js';
+import define from './define.js';
+import { composite, singleton, decorable, aggregate } from './types.js';
+
+export default {
+  create,
+  define,
+  composite,
+  singleton,
+  decorable,
+  aggregate
+};
