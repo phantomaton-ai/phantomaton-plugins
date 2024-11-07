b260729c44dd351b192340989f5c2739c144919e
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-06 16:57:26 -0800

    Updated by Phantomaton

--- phantomaton-plugins.js
+++ phantomaton-plugins.js
@@ -1,8 +1,10 @@
 import sigilium from 'sigilium';
+import priestess from 'priestess';
 
 import create from './create.js';
 import define from './define.js';
 
 const { composite, singleton } = sigilium;
+const { start, input } = priestess;
 
-export default { create, define, composite, singleton };
+export default { create, define, composite, singleton, start, input };
