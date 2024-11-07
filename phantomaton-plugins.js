45a39007c6a15df29c5f5a2ae645b57deb0414f6
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-06 16:16:13 -0800

    fix-import

--- phantomaton-plugins.js
+++ phantomaton-plugins.js
@@ -1,10 +1,8 @@
-import { composite, singleton } from 'sigilium';
+import sigilium from 'sigilium';
+
 import create from './create.js';
 import define from './define.js';
 
-export default {
-  create,
-  define,
-  composite,
-  singleton
-};
+const { composite, singleton } = sigilium;
+
+export default { create, define, composite, singleton };
