a0db3909b682295065a35998fbc5baf9ab5803a1
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-11-06 16:15:11 -0800

    expose

--- create.js
+++ create.js
@@ -2,13 +2,20 @@ import expose from './expose.js';
 
 const create = (components, instantiate, install) => {
   const extensions = expose(components);
-  return configuration => {
+  const plugin = configuration => {
     const instance = instantiate(configuration);
     return {
-      install: install({ configuration, extensions, instance }),
+      install: [
+        ...Object.values(extensions).map(extension => extension.resolver()),
+        ...install({ configuration, extensions, instance })
+      ],
       instance
     };
   };
+  Object.keys(extensions).forEach(key => {
+    plugin[key] = extensions[key];
+  });
+  return plugin;
 };
 
 export default create;
